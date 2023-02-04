1189efc7afe68cadfddb4c3d1de860da671b6428
thisSasha <trhfjfn@gmail.com>
2023-02-04 16:08:50 +0300

    backup

--- js/mainScript.js
+++ js/mainScript.js
@@ -2,7 +2,19 @@ setInterval(() => {
     if(document.querySelector('.text_c92e')!=null){
         document.querySelector('.text_c92e').innerHTML = 'У вас есть какие-то вопросы?';
     };
-}, 5000);
-
-
+}, 500);
+let video = document.querySelector('video');
+let videoPopup = document.querySelector('.popupVideo');
+videoPopup.onclick = function(e){
+    if(e.target.classList.contains('popupVideo')){
+        videoPopup.style.display = 'none';
+        video.pause();
+        video.currentTime = 0;
+    };
+};
+video.onended = function() {
+    videoPopup.style.display = 'none';
+    video.pause();
+    video.currentTime = 0; 
+};
 document.body.style.fontSize = document.body.offsetWidth/102.2+'px';
